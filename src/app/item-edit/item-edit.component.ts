0963e385c734c55f16d81cdf150802cfbaab0175
tomekbielawa <tomek.bielawa@gmail.com>
2021-06-13 19:25:15 +0200

    Imports clean up

--- src/app/item-edit/item-edit.component.ts
+++ src/app/item-edit/item-edit.component.ts
@@ -1,9 +1,4 @@
-import {
-  ChangeDetectionStrategy,
-  ChangeDetectorRef,
-  Component,
-  OnInit
-} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {
   AbstractControl,
   FormControl,
